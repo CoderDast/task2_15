4f8a97fbdff0927c93ab2c6c5eabadf2a02afeb6
CoderDast <dastsultan@gmail.com>
2020-01-13 19:43:49 +0600

    task2_15

---
+++ task2_15.py
@@ -0,0 +1,8 @@
+age_n = int(input('Your age = '))
+for i in range(age_n + 1):
+    if age_n % 2 == 0:
+        if i % 2 == 0:
+            print(i)
+    elif age_n % 2 == 1:
+        if i % 2 ==1:
+            print(i)
